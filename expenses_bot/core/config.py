6c06282680ebe0c69ddd805dcef7a6a6c8bcbc22
artemdorozhkin <aa.dorozhkin@ya.ru>
2025-06-26 22:48:25 +0300

    Add ADMIN_ID in config

--- expenses_bot/core/config.py
+++ expenses_bot/core/config.py
@@ -1,1 +1,14 @@
+import os
+import sys
+
+
 DB_FILE = "db/expenses.db"
+try:
+    admin_id = os.getenv("ADMIN")
+    if not admin_id:
+        raise ValueError()
+    ADMIN_ID = int(admin_id)
+except ValueError:
+    ADMIN_ID = -1
+    print("ERR: you need specify ADMIN env variable")
+    sys.exit(1)
